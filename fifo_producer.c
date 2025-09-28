1773e356383e1a926db0831e77f690a31fc5a335
Didier Carrillo <didiercarrillo40@gmail.com>
2025-09-28 08:51:59 -0500

    created FIFO and opened it for writing

--- fifo_producer.c
+++ fifo_producer.c
@@ -16,12 +16,12 @@ main()
   int num, fd;
 
   /* create a FIFO special file with name FIFO_NAME */
-
+  mkfifo(FIFO_NAME, 0666);
 
   /* open the FIFO file for writing. open() blocks for readers */
   printf("waiting for readers...");
   fflush(stdout);
-
+  fd = open(FIFO_NAME, O_WRONLY);
   printf("got a reader !\n");
   
   printf("Enter text to write in the FIFO file: ");
