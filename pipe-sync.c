165cac4290bd4af7bd43d75e3918ef14bd64c372
Didier Carrillo <didiercarrillo40@gmail.com>
2025-09-29 10:27:30 -0500

    7th

--- pipe-sync.c
+++ pipe-sync.c
@@ -19,21 +19,21 @@
 int main()
 {
   char *s, buf[1024];
-  int fds[2];
+  int fds[2], fds2[2];
   int ret, stat;
   s  = "Use Pipe for Process Synchronization\n";
 
   /* create pipe */
   pipe(fds);
-
+  pipe(fds2);
   ret = fork();
   if (ret == 0) {
 
     /* child process. */
     //write(fds[1], buf, strlen(s));
     printf("Child line 1\n");
-    //write(fds[1], buf, strlen(s));
-    read(fds[0], buf, strlen(s));
+    write(fds[1], buf, strlen(s));
+    read(fds2[0], buf, strlen(s));
     printf("Child line 2\n");
     write(fds[1], buf, strlen(s));
   } else {
@@ -41,7 +41,7 @@ int main()
     /* parent process */
     read(fds[0], buf, strlen(s));
     printf("Parent line 1\n");
-    read(fds[0], buf, strlen(s));
+    write(fds2[0], buf, strlen(s));
     printf("Parent line 2\n");
 
     wait(&stat);
